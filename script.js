5d6b227b5220890f56b85383314d15754db2b74c
Lukas <elrippo.zeil@gmail.com>
2025-01-06 15:53:06 +0100

    added preSelectUpcomingRace function

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let selectBetP10;
 let selectBetFirstDnf;
 let raceList = [];
 let driverList = [];
+let currentDate = new Date(); // Erzeugt ein Date-Objekt
 
 document.addEventListener("DOMContentLoaded", function () {
-    loadRaceList();
-    loadDriverList();
-
     chooseRaceDiv = document.getElementById("chooseRaceDiv");
     selectBetRaceWinner = document.getElementById("betRaceWinner");
     selectBetP10 = document.getElementById("betP10");
     selectBetFirstDnf = document.getElementById("betFirstDnf");
-});
 
+    loadRaceList();
+    loadDriverList();
+});
 
 
 function loadRaceList() {
@@ -26,10 +26,11 @@ function loadRaceList() {
             return response.json();
         })
         .then(data => {
-            console.log("Race list loaded:", data);
+            //console.log("Race list loaded:", data);
             // Verarbeite hier die geladene Race-Liste
-            raceList=data;
+            raceList = data;
             displayRaces();
+            preSelectUpcomingRace();
             addRadioClickListeners();
         })
         .catch(error => {
@@ -46,9 +47,9 @@ function loadDriverList() {
             return response.json();
         })
         .then(data => {
-            console.log("Driver list loaded:", data);
+            //console.log("Driver list loaded:", data);
             // Verarbeite hier die geladene Race-Liste
-            driverList=data;
+            driverList = data;
             fillDropdowns();
         })
         .catch(error => {
@@ -56,7 +57,7 @@ function loadDriverList() {
         });
 }
 
-function displayRaces(){
+function displayRaces() {
     raceList.races.forEach(race => {
         // Durch Objekt iterieren
         /*
@@ -79,10 +80,10 @@ function displayRaces(){
         input.type = "radio";
         input.name = "chosenRace"; // Gruppe für Rennen-Auswahl
         input.value = race.number; // race.id wird für den Wert verwendet
-        input.id = race.number;   // ID für das Rennen
+        input.id = "chosenRaceRadio" + race.number;   // ID für das Rennen
 
         const label = document.createElement("label");
-        label.setAttribute("for", race.number); // Verknüpfe Label mit der Input-ID
+        label.setAttribute("for", "chosenRaceRadio" + race.number); // Verknüpfe Label mit der Input-ID
 
         const img = document.createElement("img");
         img.src = "assets/graphics/flag-icons/" + race.country_imgPath.toLowerCase() + ".svg"; // Annahme: `race.flagIcon` enthält den Pfad zur Flaggen-Grafik des Rennens
@@ -102,7 +103,7 @@ function fillDropdowns() {
 
     driverList.teams.forEach(team => {
         team.drivers.forEach(driver => {
-            console.log(driver.name);
+            //console.log(driver.name);
             selectBetRaceWinner.appendChild(new Option(driver.name + " " + driver.number, driver.number));
             selectBetP10.appendChild(new Option(driver.name + " " + driver.number, driver.number));
             selectBetFirstDnf.appendChild(new Option(driver.name + " " + driver.number, driver.number));
@@ -125,12 +126,30 @@ function addRadioClickListeners() {
 
 // Funktion, die bei jedem Klick auf einen Radiobutton ausgeführt wird
 function handleRadioClick(radioButton) {
-    console.log(`Radio-Button mit Wert "${radioButton.value}" wurde gewählt.`);
-    console.log(raceList.races[radioButton.value-1].country);
-    document.getElementById('raceCountry').innerText = raceList.races[radioButton.value-1].country;
-    document.getElementById('raceDayDate').innerText = raceList.races[radioButton.value-1].endDate;
-    document.getElementById('raceStartTime').innerText = raceList.races[radioButton.value-1].start_time + " Uhr in Österreich";
-    document.getElementById('locationName').innerText = raceList.races[radioButton.value-1].location;
-    document.getElementById('trackName').innerText = raceList.races[radioButton.value-1].circuit;
-    document.getElementById('trackLength').innerText = "Strecken Länge: " + raceList.races[radioButton.value-1].length_km + " km";
+    //console.log(`Radio-Button mit Wert "${radioButton.value}" wurde gewählt.`);
+    //console.log(raceList.races[radioButton.value - 1].country);
+    let raceDayDate = new Date(raceList.races[radioButton.value - 1].endDate);
+
+    document.getElementById('raceCountry').innerText = raceList.races[radioButton.value - 1].country;
+    document.getElementById('raceDayDate').innerText = raceDayDate.toLocaleDateString("de-DE");
+    document.getElementById('raceStartTime').innerText = raceList.races[radioButton.value - 1].start_time + " Uhr in Österreich";
+    document.getElementById('locationName').innerText = raceList.races[radioButton.value - 1].location;
+    document.getElementById('trackName').innerText = raceList.races[radioButton.value - 1].circuit;
+    document.getElementById('trackLength').innerText = "Strecken Länge: " + raceList.races[radioButton.value - 1].length_km + " km";
+}
+
+function preSelectUpcomingRace() {
+    let racesInPast = 1;
+    raceList.races.forEach(race => {
+        // Durch Objekt iterieren
+        if (new Date(new Date(race.endDate).setHours(24,0,0,0)) <= currentDate) {
+            racesInPast++;
+        }
+    });
+
+    //Radiobutton des aktuellen Rennen auswählen
+    let radioToSelect = document.getElementById('chosenRaceRadio' + racesInPast);
+    radioToSelect.click();
+    handleRadioClick(radioToSelect);
+    console.log("automatically changed selected Race");
 }
